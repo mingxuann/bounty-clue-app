0d456ace039ee7188b5245576f441434cb5b4d0a
zhangsiyuan3 <18518480614@163.com>
2022-12-09 13:45:49 +0800

    home页提交

--- src/views/home.vue
+++ src/views/home.vue
@@ -14,6 +14,7 @@ const changeState = () => {
     const blobList = document.querySelectorAll('.blob')
     create(blobList[random(0, blobList.length)])
 }
+
 let offset = 25
 const create = (blob) => {
     let r = []
